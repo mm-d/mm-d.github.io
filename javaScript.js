55d4470bc5c7931f8c95373509fea5361be94fd8
mm-d <doraemonmk3@gmail.com>
2022-03-22 17:14:46 +0900

    a

---
+++ javaScript.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+  menu = document.getElementsByClassName('mdl-menu__item');
+  console.log(menu);
+}
+function menu1(){
+  window.location.href='#index';
+}
+function menu2(){
+
+}
+function menu3(){
+
+}
+function menu4(){
+
+}
