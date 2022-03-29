45aaaa5cf167ccd0e66b2ec0ec81b70dfd2250d5
mm-d <doraemonmk3@gmail.com>
2022-03-29 15:29:40 +0900

    a

--- javaScript.js
+++ javaScript.js
@@ -3,14 +3,14 @@ window.onload = function(){
   console.log(menu);
 }
 function menu1(){
-  window.location.href='#index';
+
 }
 function menu2(){
 
 }
 function menu3(){
-
+  window.location.href='https://getmdl.io/';
 }
 function menu4(){
-
+  window.location.href='https://github.com/google/material-design-lite/tree/mdl-1.x/templates/blog';
 }
